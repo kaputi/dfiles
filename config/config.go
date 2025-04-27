2fb4d232299f31ea6c9f62c36a8e3f35e11b32f0
Eduardo Lonighi <edu_loni3@hotmail.com>
2025-04-27 06:58:20 +0100

    config

--- config/config.go
+++ config/config.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 type Config struct {
@@ -11,31 +12,44 @@ type Config struct {
 	Exclude []string `json:"exclude"`
 }
 
-var configFile = os.Getenv("USER") + ".local/share/dfiles/config.json"
+func newConfig() Config {
+	return Config{
+		Include: []string{},
+		Exclude: []string{},
+	}
+}
+
+var configFile = os.Getenv("HOME") + "/.local/share/dfiles/config.json"
 
 func ReadConfig() Config {
 	byteValue, err := os.ReadFile(configFile)
 
 	if err != nil {
-		log.Fatal(err)
+		return newConfig()
 	}
 
 	var config Config
 	err = json.Unmarshal(byteValue, &config)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error parsing config file: %v", err)
 	}
 
 	return config
 }
 
 func WriteConfig(config Config) {
+	dir := filepath.Dir(configFile)
+	err := os.MkdirAll(dir, 0755)
+	if err != nil {
+		log.Fatalf("Error creating directories: %v", err)
+	}
+
 	byteValue, err := json.MarshalIndent(config, "", "  ")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error encoding config file: %v", err)
 	}
 	err = os.WriteFile(configFile, byteValue, 0644)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error writing config file: %v", err)
 	}
 }
